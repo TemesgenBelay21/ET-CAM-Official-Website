43e86bcea00a9506e5aaa9f572b9e41489cbe0b8
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-09 06:50:13 +0100

    feat: derive language from URL path for bilingual pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './i18n/LanguageContext.jsx'
+import { LANGS } from './lib/head.js'
+
+const defaultLang = window.location.pathname.startsWith('/am') ? 'am' : 'en'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LanguageProvider>
+    <LanguageProvider defaultLang={LANGS.includes(defaultLang) ? defaultLang : 'en'}>
       <App />
     </LanguageProvider>
   </StrictMode>,
